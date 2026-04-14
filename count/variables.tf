71fcba5d4341d78b1e1cb6c8a5ae118cd0c6f3d2
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-04-14 20:58:15 +0530

    Terraform

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+  default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+  default = "Z00990443C66P00HIY8Z0"
+}
+
+variable "domain_name" {
+  default = "dawsc86s.online"
+}
