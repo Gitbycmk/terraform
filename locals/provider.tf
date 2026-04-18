6fec620223548737255c80d96dfe482b0f8f65b6
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-04-18 14:29:38 +0530

    Terraform

---
+++ locals/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-86s-remote" 
+    key = "locals-86s-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ locals/variable.tf
@@ -0,0 +1,68 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+# variable "common_name" {
+#   default = "${var.project}-${var.environment}"
+# }
+
+
+variable "ami_id" {
+  type = string
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "common_tags" {
+  type = map
+  default = {
+        Terraform = "true"
+        Project = "roboshop"
+        Env = ""
+  }
+}
+
+variable "sg_name" {
+  type = string
+  default = "allow_all_demo"
+  # (description) --> Optional to inform what is this variable about
+  description = " Security Group Name to attach to EC2 Instance"
+}
+
+variable "cidr" {
+  type = list 
+  default = ["0.0.0.0/0"]
+}
+
+variable "ingress_from_port" {
+  type = number
+  default = 0
+}
+
+variable "ingress_to_port" {
+  type = number
+  default = 0
+}
+
+variable "egress_from_port" {
+  type = number
+  default = 0
+}
+
+variable "egress_to_port" {
+  type = number
+  default = 0
+}
+
+variable "protocol" {
+  type = string
+  default = "-1"
+}
